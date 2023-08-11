172aee8374320a60a5242ca43ebc1173f12f5cae
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-10 21:16:59 -0500

    edit task form, update API, display result

--- public/edit-tasks.js
+++ public/edit-tasks.js
@@ -27,3 +27,41 @@ const showTask = async () => {
 };
 
 showTask();
+
+editFormDOM.addEventListener("submit", async (e) => {
+  editBtnDOM.textContent = "Loading...";
+  e.preventDefault();
+  try {
+    const taskName = taskNameDOM.value;
+    const taskCompleted = taskCompletedDOM.checked;
+
+    const {
+      data: { task },
+    } = await axios.patch(`/api/v1/tasks/${id}`, {
+      name: taskName,
+      completed: taskCompleted,
+    });
+
+    const { _id: taskID, completed, name } = task;
+
+    taskIDDOM.textContent = taskID;
+    taskNameDOM.value = name;
+    tempName = name;
+    if (completed) {
+      taskCompletedDOM.checked = true;
+    }
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = `success, edited task`;
+    formAlertDOM.classList.add("text-success");
+  } catch (error) {
+    console.error(error);
+    taskNameDOM.value = tempName;
+    formAlertDOM.style.display = "block";
+    formAlertDOM.innerHTML = `error, please try again`;
+  }
+  editBtnDOM.textContent = "Edit";
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+    formAlertDOM.classList.remove("text-success");
+  }, 3000);
+});
