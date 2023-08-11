6559d14e7d8618203361581f3900138a23e7004c
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-10 21:13:58 -0500

    Add submit event listener to form

--- public/browser-app.js
+++ public/browser-app.js
@@ -62,3 +62,26 @@ tasksDOM.addEventListener("click", async (e) => {
   }
   loadingDOM.style.visibility = "hidden";
 });
+
+// form
+
+formDOM.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = taskInputDOM.value;
+
+  try {
+    await axios.post("/api/v1/tasks", { name });
+    showTasks();
+    taskInputDOM.value = "";
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = `success, task added`;
+    formAlertDOM.classList.add("text-success");
+  } catch (error) {
+    formAlertDOM.style.display = "block";
+    formAlertDOM.innerHTML = `error, please try again`;
+  }
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+    formAlertDOM.classList.remove("text-success");
+  }, 3000);
+});
