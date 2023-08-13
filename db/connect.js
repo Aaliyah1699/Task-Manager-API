9658e6a10e509c75d070d3889931831c092bdb83
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-13 16:41:31 -0500

    use mongoose to connect to db

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,6 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://adestanym:<9wUUreqSIAgpjcJR>@task-manager-api.syv7yai.mongodb.net/Task-Manager-API?retryWrites=true&w=majority";
+  "mongodb+srv://adestanym:9wUUreqSIAgpjcJR@task-manager-api.syv7yai.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString);
