836903ea31c9ab97a41cae3196db89c02cca2b6c
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-13 16:46:25 -0500

    fixed bug in connection

--- db/connect.js
+++ db/connect.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://adestanym:9wUUreqSIAgpjcJR@task-manager-api.syv7yai.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
 
-mongoose.connect(connectionString).then(() => {
-  console.log("Connected to DB").catch((err) => {
-    console.log(err);
-  });
-});
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
