2ff0b94cdd4c24300eb5b1db462f1cfe978ecea8
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-14 00:44:00 -0500

    refactor controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,17 @@
 // controllers
+const Task = require("../models/Tasks");
 
 const getAllTasks = (req, res) => {
   res.send("get all tasks");
 };
 
-const createTask = (req, res) => {
-  res.send("create task");
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 
 const getTask = (req, res) => {
-//   res.send("get single task");
+  //   res.send("get single task");
   res.json({ id: req.params.id });
 };
 
