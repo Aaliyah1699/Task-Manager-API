d77c6140415c3bd3689b80ee9b28e3c6ff6590ca
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-14 00:35:33 -0500

    fixed bug

--- models/Tasks.js
+++ models/Tasks.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
   name: String,
-  complicated: Boolean,
+  completed: Boolean,
 });
 
-module.exports = mongoose.model('Task',TaskSchema)
+module.exports = mongoose.model("Task", TaskSchema);
