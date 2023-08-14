ba12396fc8461a4723554134b00584d499eb4797
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-14 18:28:19 -0500

    refactor route to get single item

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -19,9 +19,19 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  //   res.send("get single task");
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+    
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
@@ -29,6 +39,7 @@ const updateTask = (req, res) => {
 };
 
 const deleteTask = (req, res) => {
+
   res.send("delete task");
 };
 module.exports = {
