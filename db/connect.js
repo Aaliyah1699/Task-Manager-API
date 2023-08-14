ec3db2a3e59337cc85d148173f6a267fd540a218
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-13 23:44:10 -0500

    set up db with dotenv

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://adestanym:9wUUreqSIAgpjcJR@task-manager-api.syv7yai.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
-  return mongoose.connect(connectionString);
+  return mongoose.connect(url);
 };
 
 module.exports = connectDB;
